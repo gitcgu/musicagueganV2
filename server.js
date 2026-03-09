f654700f3e227678bc2db61696a69f3b370b6dd0
gitcgu <wlguegan@gmail.com>
2026-03-09 20:42:22 +0100

    Update server.js fix for mix ...

--- server.js
+++ server.js
@@ -79,10 +79,16 @@ app.get('/api/file/:type/:bucketType/:fileName', async (req, res) => {
     const [exists] = await file.exists();
     if (!exists) return res.status(404).json({ error: 'Fichier non trouvé' });
     
-    const [content] = await file.download();
+    //const [content] = await file.download();
     res.setHeader('Content-Type', contentType);
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.send(content);
+    const stream = file.createReadStream();
+    stream.on('error', err => {
+      console.error('Stream error:', err);
+      res.status(500).end();
+      });
+    stream.pipe(res);
   } catch (e) {
     console.error('Erreur fichier:', e);
     res.status(500).json({ error: 'Erreur serveur' });
