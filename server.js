aede4a5207fea5572f0d7f8b516a72f12a7c9a85
gitcgu <wlguegan@gmail.com>
2026-04-03 11:21:27 +0200

    Update server.js test other gemini

--- server.js
+++ server.js
@@ -353,7 +353,8 @@ app.get('/api/file/audio/mix/:name', async (req, res) => {
 async function generateSongDescription(songName) {
   try {
     const response = await fetch(
-      `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=${API_KEY}`,
+     // `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=${API_KEY}`,
+      `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.0-pro:generateContent?key=${API_KEY}`,
       {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
