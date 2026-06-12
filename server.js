e0b352aee4e433e7d9c1b261145cbcc332f3e64e
gitcgu <wlguegan@gmail.com>
2026-06-12 12:04:22 +0200

    Update server.js
    vertex ai from us-central to europe

--- server.js
+++ server.js
@@ -22,8 +22,8 @@ const DEFAULT_POCHETTE_URL = `https://storage.googleapis.com/${MP3_BUCKET_NAME}/
 const { VertexAI } = require('@google-cloud/vertexai');
 const vertexAI = new VertexAI({
   project: '836359398199',  // Remplace par ton project GCP
-//  location: 'europe-west1',
-    location: 'us-central1',  // ✅ CHANGE JUSTE CETTE LIGNE (au lieu de europe-west1)    ok=h year
+  location: 'europe-west1',
+//    location: 'us-central1',  // ✅ CHANGE JUSTE CETTE LIGNE (au lieu de europe-west1)    ok=h year
 });
 
 // Fonction pour générer description VERTEX
