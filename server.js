9ae5bf921d1168475f44c55d4911a27e85612511
gitcgu <wlguegan@gmail.com>
2026-06-29 10:46:14 +0200

    Update server.js
    waveformJsonUrl: `/api/waveform/mix/${encodeURIComponent(mix)}`

--- server.js
+++ server.js
@@ -300,7 +300,8 @@ app.get('/api/mix-list', async (req, res) => {
       name: mix.replace('.mp3', ''),
       fileName: mix,
       url: `/api/file/audio/mix/${encodeURIComponent(mix)}`,
-      waveformJsonUrl: `/api/waveform/mix/${encodeURIComponent(mix.replace('.mp3', ''))}`
+      //waveformJsonUrl: `/api/waveform/mix/${encodeURIComponent(mix.replace('.mp3', ''))}`
+      waveformJsonUrl: `/api/waveform/mix/${encodeURIComponent(mix)}`
 
     }));
 
